a153c2e9aab329975f6150d9f76085a5fb52f7be
Trocoli <ygoraraujoca@outlook.com>
2023-10-30 15:16:37 -0300

    api started

---
+++ prisma/migrations/20231030161938_issue/migration.sql
@@ -0,0 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
+
+-- CreateTable
+CREATE TABLE "Issue" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'OPEN',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Issue_pkey" PRIMARY KEY ("id")
+);
---
+++ src/app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/../prisma/client";
+
+const issueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validatedObj = issueSchema.safeParse(body);
+  if (!validatedObj.success) {
+    return NextResponse.json(validatedObj.error.errors, { status: 400 });
+  }
+  const newIssue = await prisma.issues.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
